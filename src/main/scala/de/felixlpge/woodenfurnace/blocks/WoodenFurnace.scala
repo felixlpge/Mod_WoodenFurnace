699579f5d329dc7d8f42f5935768ec89d9c6261e
felix <felixletsplay@outlook.de>
2018-09-23 12:07:07 +0200

    Initial commit

---
+++ src/main/scala/de/felixlpge/woodenfurnace/blocks/WoodenFurnace.scala
@@ -0,0 +1,11 @@
+package de.felixlpge.woodenfurnace.blocks
+
+import net.minecraft.block.Block
+import net.minecraft.block.material.Material
+import net.minecraft.creativetab.CreativeTabs
+
+class WoodenFurnace extends Block(Material.GROUND) {
+  setUnlocalizedName("woodenfurnace")
+  setRegistryName("woodenfurnace")
+  setCreativeTab(CreativeTabs.MISC)
+}
---
+++ src/main/scala/de/felixlpge/woodenfurnace/woodenfurnace.scala
@@ -0,0 +1,30 @@
+package de.felixlpge.woodenfurnace
+
+import de.felixlpge.woodenfurnace.blocks.WoodenFurnace
+import net.minecraft.block.Block
+import net.minecraftforge.event.RegistryEvent
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+@Mod(modid = woodenfurnace.MODID, version = woodenfurnace.VERSION, modLanguage = "scala")
+object woodenfurnace {
+  final val MODID = "woodenfurnace"
+  final val VERSION = "1.0"
+  @EventHandler
+  def preInit(event: FMLPreInitializationEvent): Unit ={
+
+  }
+
+
+
+}
+@Mod.EventBusSubscriber(modid = woodenfurnace.MODID)
+object RegistrationHandler{
+  @SubscribeEvent
+  def registerBlocks(event: RegistryEvent.Register[Block]): Unit ={
+    val furnace = new WoodenFurnace
+    event.getRegistry.register(furnace)
+  }
+}
